9dee907a78e9ae5da167988e7602ddf8e911802c
ZiHao Zhou <49811805+ZZH0@users.noreply.github.com>
2020-05-04 22:16:30 +0800

    Update Multi scale detail enhence.cpp

--- Traditional image enhancement/Multi scale detail enhence.cpp
+++ Traditional image enhancement/Multi scale detail enhence.cpp
@@ -24,21 +24,21 @@ Mat MultiScaleDetailBoosting(Mat src, int Radius)
     int cols = src.cols;
     Mat B1, B2, B3;
 
-  	GaussianBlur(src, B1, Size(Radius, Radius), 1.0, 1.0);           //¸ßË¹Ä£ºı, ¸ßË¹ÄÚºËÔÚX·½ÏòµÄ±ê×¼Æ«²îÎª1.0£¬¸ßË¹ÄÚºËÔÚY·½ÏòµÄ±ê×¼Æ«²îÒ²ÊÇ1.0
-	GaussianBlur(src, B2, Size(Radius*2-1, Radius*2-1), 2.0, 2.0);   //¸ßË¹ÄÚºËÔÚX·½ÏòµÄ±ê×¼Æ«²îÎª2.0£¬¸ßË¹ÄÚºËÔÚY·½ÏòµÄ±ê×¼Æ«²îÎª2.0
-	GaussianBlur(src, B3, Size(Radius*4-1, Radius*4-1), 4.0, 4.0);
+    GaussianBlur(src, B1, Size(Radius, Radius), 1.0, 1.0);           //é«˜æ–¯æ¨¡ç³Š, é«˜æ–¯å†…æ ¸åœ¨Xæ–¹å‘çš„æ ‡å‡†åå·®ä¸º1.0ï¼Œé«˜æ–¯å†…æ ¸åœ¨Yæ–¹å‘çš„æ ‡å‡†åå·®ä¹Ÿæ˜¯1.0
+    GaussianBlur(src, B2, Size(Radius*2-1, Radius*2-1), 2.0, 2.0);   //é«˜æ–¯å†…æ ¸åœ¨Xæ–¹å‘çš„æ ‡å‡†åå·®ä¸º2.0ï¼Œé«˜æ–¯å†…æ ¸åœ¨Yæ–¹å‘çš„æ ‡å‡†åå·®ä¸º2.0
+    GaussianBlur(src, B3, Size(Radius*4-1, Radius*4-1), 4.0, 4.0);
 	
 
-    float w1 = 0.5, w2 = 0.5, w3 = 0.25;                             //ÂÛÎÄ¸ø³ö
+    float w1 = 0.5, w2 = 0.5, w3 = 0.25;                             //è®ºæ–‡ç»™å‡º
 
     Mat dst(rows, cols, CV_8UC3);
 
     for(int i = 0; i < rows; i++)
-	{
+    {
         for(int j = 0; j < cols; j++)
-		{
+	{
             for(int k = 0; k < 3; k++)
-			{
+	    {
                 int D1 = src.at<Vec3b>(i, j)[k] - B1.at<Vec3b>(i, j)[k];  //D1 = I* - B1
                 int D2 = B1.at<Vec3b>(i, j)[k] - B2.at<Vec3b>(i, j)[k];   //D2 = B1 - B2
                 int D3 = B2.at<Vec3b>(i, j)[k] - B3.at<Vec3b>(i, j)[k];   //D3 = B2 - B3
